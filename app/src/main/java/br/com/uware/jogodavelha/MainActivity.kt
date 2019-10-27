89cc0f54ed77d1b1c84e2f6a62b8b795c62e04a1
Rodrigo Leutz <rodrigo@uware.com.br>
2019-10-27 19:13:48 -0300

    Contra o computador

--- app/src/main/java/br/com/uware/jogodavelha/MainActivity.kt
+++ app/src/main/java/br/com/uware/jogodavelha/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var fimJogo: Boolean = false
     var jogador: Int = 1
     private var btnList = ArrayList<Button>()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +26,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun joga(btn: Button){
+        marca(btn)
+        if(jogador<9 && !checkGame()) jogaComp()
+    }
+    private fun marca(btn: Button){
         if(jogador % 2 != 0) {
             btn.text = "X"
         }
@@ -32,7 +38,20 @@ class MainActivity : AppCompatActivity() {
         }
         btn.isClickable = false
         checkGame()
-        ++jogador
+        jogador++
+    }
+    private fun jogaComp(){
+        var bol: Boolean = true
+        do {
+            var rand = Random.nextInt(9)
+            if (btnList[rand].text == "") {
+                marca(btnList[rand])
+                bol = false
+            }
+            else {
+               bol = true
+            }
+        }while (bol)
     }
     private fun restart(){
         for(x in 0..btnList.size-1){
@@ -41,38 +60,46 @@ class MainActivity : AppCompatActivity() {
         }
         tvResult.text = ""
         jogador = 1
+        fimJogo = false
         btnRestart.visibility = View.GONE
     }
-    private fun checkGame(){
-        if(jogador == 9){
-            tvResult.text = "Empate"
-            btnRestart.visibility = View.VISIBLE
-        }
+    private fun checkGame(): Boolean{
         val numb = listOf<Int>(0,3,6)
         for(x in numb){
             if (btnList[x].text != "" && btnList[x].text == btnList[x+1].text && btnList[x+1].text == btnList[x + 2].text) {
                 ganhador()
+                return true
             }
         }
         for(x in 0..2){
             if(btnList[x].text != "" && btnList[x].text == btnList[x+3].text && btnList[x+3].text == btnList[x+6].text){
                 ganhador()
+                return true
             }
         }
         if ((btnList[0].text != "" && btnList[0].text == btnList[4].text && btnList[4].text == btnList[8].text)
             || (btnList[2].text != "" && btnList[2].text == btnList[4].text && btnList[4].text == btnList[6].text)) {
             ganhador()
+            return true
         }
+        else if(jogador == 9){
+            tvResult.text = "Empate"
+            btnRestart.visibility = View.VISIBLE
+            return true
+        }
+        return false
     }
     private fun ganhador(){
-        if(jogador % 2 != 0){
-            tvResult.text = "Ganhador: X"
+        if(!fimJogo) {
+            if (jogador % 2 != 0) {
+                tvResult.text = "Ganhador: X"
+            } else {
+                tvResult.text = "Ganhador: O"
+            }
+            endGame()
+            btnRestart.visibility = View.VISIBLE
+            fimJogo = true
         }
-        else{
-            tvResult.text = "Ganhador: O"
-        }
-        endGame()
-        btnRestart.visibility = View.VISIBLE
     }
     private fun endGame(){
         for(x in 0..btnList.size-1){
