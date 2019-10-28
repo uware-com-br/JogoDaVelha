c941934cdea75184b704fb3b6fb58d9fbeb06f16
Rodrigo Leutz <rodrigo@uware.com.br>
2019-10-28 02:26:01 -0300

    Computador escolhe

--- app/src/main/java/br/com/uware/jogodavelha/MainActivity.kt
+++ app/src/main/java/br/com/uware/jogodavelha/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.uware.jogodavelha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
@@ -41,17 +42,58 @@ class MainActivity : AppCompatActivity() {
         jogador++
     }
     private fun jogaComp(){
-        var bol: Boolean = true
-        do {
-            var rand = Random.nextInt(9)
-            if (btnList[rand].text == "") {
-                marca(btnList[rand])
-                bol = false
+        if(checkPos() != -1){
+            Log.d("Posicao: ",checkPos().toString())
+            marca(btnList[checkPos()])
+        }
+        else {
+            var bol: Boolean = true
+            do {
+                var rand = Random.nextInt(9)
+                if (btnList[rand].text == "") {
+                    marca(btnList[rand])
+                    bol = false
+                } else {
+                    bol = true
+                }
+            } while (bol)
+        }
+    }
+    private fun checkPos(): Int{
+        for(x in 0..2){
+            if(btnList[x].text != "" || btnList[x+3].text != "" || btnList[x+6].text != "") {
+                if (btnList[x].text == btnList[x + 3].text && btnList[x + 6].text == "") {
+                    return x + 6
+                }
+                if (btnList[x + 6].text == btnList[x + 3].text && btnList[x].text == "") {
+                    return x
+                }
+                if (btnList[x + 6].text == btnList[x].text && btnList[x + 3].text == "") {
+                    return x + 3
+                }
             }
-            else {
-               bol = true
+        }
+        var numb = listOf<Int>(0,3,6)
+        for(x in numb){
+            if(btnList[x].text != "" || btnList[x+2].text != "" || btnList[x+2].text != "") {
+                if (btnList[x].text == btnList[x + 1].text && btnList[x + 2].text == "") {
+                    return x + 2
+                }
+                if (btnList[x + 2].text == btnList[x + 1].text && btnList[x].text == "") {
+                    return x
+                }
+                if (btnList[x].text == btnList[x + 2].text && btnList[x + 1].text == "") {
+                    return x + 1
+                }
             }
-        }while (bol)
+        }
+        if(btnList[0].text != "" && btnList[0].text == btnList[4].text && btnList[8].text == "") return 8
+        if(btnList[4].text != "" && btnList[4].text == btnList[8].text && btnList[0].text == "") return 0
+        if(btnList[0].text != "" && btnList[0].text == btnList[4].text && btnList[4].text == "") return 4
+        if(btnList[2].text != "" && btnList[2].text == btnList[4].text && btnList[6].text == "") return 6
+        if(btnList[2].text != "" && btnList[2].text == btnList[6].text && btnList[4].text == "") return 4
+        if(btnList[4].text != "" && btnList[4].text == btnList[6].text && btnList[2].text == "") return 2
+        return -1
     }
     private fun restart(){
         for(x in 0..btnList.size-1){
